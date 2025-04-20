6ff2dc00cda2a10fdac4218e7fa3fe427757a7fc
54-xenon <congas.rabbi_0b@icloud.com>
2025-04-20 20:43:04 +0900

    Gitのキャッシュクリア

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../models/categori_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,10 +9,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  List<CategoriModel> categoris = [];
+  // List<CategoriModel> categoris = [];
 
   void _getInfoMethod() {
-    categoris = CategoriModel.getCategories();
+    
   }
 
   @override
@@ -28,7 +27,27 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.white,
             )
         ),
-        backgroundColor: Colors.black,
+        leading: Container(
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        actions: [
+          Container(
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+                color: Colors.white,
+                ),
+            ),
+          )
+        ],
+        backgroundColor: Colors.grey[900],
         elevation: 0,
       ),
       body: Column(
@@ -45,47 +64,6 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          SizedBox(height: 15),
-          Container(
-            height: 120,
-            child: ListView.separated(
-              // スクロール方向を横並びにする
-                scrollDirection: Axis.horizontal,
-                itemCount: categoris.length,
-                separatorBuilder: (context, index) => SizedBox(width: 20),
-                itemBuilder: (context, index) {
-                  return Container(
-                    // カテゴリUIを戻り値にする
-                    // モデルデータから引っ張ったやつ
-                    width: 100,
-                    decoration: BoxDecoration(
-                      color: categoris[index].boxColor.withValues(alpha: 0.4),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        // images
-                        Container(
-                          width: 70,
-                          height: 70,
-                          child: Image.asset(categoris[index].iconPath),
-                        ),
-                        Text(
-                          categoris[index].name,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                        // categoris text
-                      ],
-                    ),
-                  );
-                },
-            ),
-          )
         ],
       ),
     );
